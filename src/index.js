d3f997bad4030f59ffbed1a9753fd02447693c9c
kuberpatel <101393723+kuberpatel@users.noreply.github.com>
2023-09-07 11:30:42 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+// Create a root for rendering the React application into the DOM
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
+// Render the main App component into the root
 root.render(<App />);
