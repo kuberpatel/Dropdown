4854e9c8efd5c99f2de6356e6c8bc70b3dc7b913
kuberpatel <101393723+kuberpatel@users.noreply.github.com>
2023-09-07 11:29:19 +0530

    Update Dropdown.js

--- src/Dropdown.js
+++ src/Dropdown.js
@@ -1,34 +1,40 @@
 import { useState } from "react";
 
+// Define the Dropdown component
 const Dropdown = () => {
+    // Define and initialize state variables
+    const [isActive, setActive] = useState(false); // Dropdown activation state
+    const [list] = useState(['Yes', 'Probably not']); // Dropdown options list
+    const [value, setValue] = useState(''); // Selected dropdown value
 
-    const [isActive, setActive] = useState(false);
-    const [list] = useState(['Yes', 'Probably not']);
-    const [value, setValue] = useState('');
-
-    const onClickHandeler = (e) => {
-        setValue(e.target.textContent);
-        setActive(false);
+    // Event handler for option click
+    const onClickHandler = (e) => {
+        setValue(e.target.textContent); // Set the selected value
+        setActive(false); // Deactivate the dropdown
     }
 
+    // Event handler for mouse hover
     const onMouseHover = () => {
-        setActive(true);
+        setActive(true); // Activate the dropdown on hover
     }
 
+    // Create an array of option elements based on the list
     const results = list.map((result, index) => {
-        return <a href="#" onClick={onClickHandeler} key={index}>{result}</a>;
+        return <a href="#" onClick={onClickHandler} key={index}>{result}</a>;
     })
 
+    // Render the Dropdown component
     return (
         <div className="dropdown">
             <h1>Should you use a dropdown?</h1>
             <h3>{value}</h3>
             <button className="dropbtn" onMouseEnter={onMouseHover}>Select</button>
             <div className="dropdown-content" style={{ display: `${isActive ? 'block' : 'none'}` }}>
-                {results}
+                {results} {/* Render the option elements */}
             </div>
         </div>
     );
 }
 
+// Export the Dropdown component as the default export
 export default Dropdown;
